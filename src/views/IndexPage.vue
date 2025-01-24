15b3267bb78b9b78633ac6577caeb017329ae64f
brian <ay83103@gmail.com>
2025-01-24 11:23:53 +0800

    feat: footer complete

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -6,6 +6,21 @@ const products = ref()
 const productCategories = ref()
 let objectProperty = 'isDesktopSize'
 
+const socialMedias = ref([
+  {
+    url: '/images/icon-facebook.svg',
+    hoverUrl: '/images/icon-facebook-peru.svg',
+  },
+  {
+    url: '/images/icon-twitter.svg',
+    hoverUrl: '/images/icon-twitter-peru.svg',
+  },
+  {
+    url: '/images/icon-instagram.svg',
+    hoverUrl: '/images/icon-instagram-peru.svg',
+  },
+])
+
 const productInBanner = computed(() => {
   if (!products.value || products.value.length === 0) return null
 
@@ -69,17 +84,20 @@ const setDivSizeToImage = (imageUrl) => {
   const img = new Image()
 
   img.onload = function () {
-    const { width, height } = img
-
     const element = div[0]
-
     element.style.backgroundImage = `url('${imageUrl}')`
-    element.style.width = `${width}px`
-    element.style.height = `${height}px`
   }
 
   // 加載圖片
   img.src = imageUrl
+}
+
+const mouseenter = (e, socialMedia) => {
+  e.target.src = socialMedia.hoverUrl
+}
+
+const mouseleave = (e, socialMedia) => {
+  e.target.src = socialMedia.url
 }
 
 switch (getDeviceType()) {
@@ -111,7 +129,7 @@ onMounted(async () => {
     <div class="header-container">
       <div class="header">
         <img src="/images/logo.svg" alt="" class="logo" />
-        <div class="navbar white h4-manrope-bold">
+        <div class="navbar white sub-title-manrope-bold">
           <span>HOME</span>
           <span>HEADPHONES</span>
           <span>SPEAKS</span>
@@ -193,5 +211,36 @@ onMounted(async () => {
       <img src="" alt="" />
     </div>
   </main>
-  <footer></footer>
+  <footer>
+    <div class="footer-content">
+      <img src="/images/logo.svg" alt="" class="logo" />
+      <div class="navbar-footer white sub-title-manrope-bold">
+        <span>HOME</span>
+        <span>HEADPHONES</span>
+        <span>SPEAKS</span>
+        <span>EARPHONES</span>
+      </div>
+      <div class="description">
+        <span class="body-manrope-medium white"
+          >Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music
+          lovers and sound specialists who are devoted to helping you get the most out of personal
+          audio. Come and visit our demo facility - we’re open 7 days a week.</span
+        >
+      </div>
+      <div class="social-media">
+        <img
+          v-for="socialMedia in socialMedias"
+          :key="socialMedia.url"
+          :src="socialMedia.url"
+          alt=""
+          srcset=""
+          @mouseenter="mouseenter($event, socialMedia)"
+          @mouseleave="mouseleave($event, socialMedia)"
+        />
+      </div>
+      <div class="copyright">
+        <span class="white body-manrope-medium">Copyright 2021. All Rights Reserved</span>
+      </div>
+    </div>
+  </footer>
 </template>
