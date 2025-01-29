7106d2a5457029aa486323700b5a1638b0f98e17
brian <ay83103@gmail.com>
2025-01-29 18:46:57 +0800

    feat 商品分類頁 complete

--- src/views/ProductCategory.vue
+++ src/views/ProductCategory.vue
@@ -46,26 +46,73 @@ ul {
   flex-direction: column;
   gap: 160px;
   max-width: 1110px;
+  padding-top: 160px;
+  padding-bottom: 160px;
 }
 
 li {
-  display: flex;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
   gap: 125px;
 }
 
 li:nth-child(2) {
-  flex-direction: row-reverse;
+  grid-template-areas: 'content img';
+}
+
+li:nth-child(2) img {
+  grid-area: img;
+}
+
+li:nth-child(2) .product-content {
+  grid-area: content;
 }
 
 li img {
-  width: 540px;
-  height: 560px;
+  max-width: 100%;
 }
 
 .product-content {
   display: flex;
   flex-direction: column;
   gap: 16px;
+}
+
+/* Tablet */
+@media screen and (max-width: 1024px) {
+  ul {
+    max-width: 689px;
+    padding-top: 120px;
+    padding-bottom: 120px;
+    gap: 120px;
+    text-align: center;
+  }
+
+  li {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 52px;
+  }
+
+  li:nth-child(2) {
+    grid-template-areas:
+      'img'
+      'content';
+  }
+
+  .product-content {
+    align-items: center;
+  }
+}
+
+/* Mobile */
+@media screen and (max-width: 767px) {
+  ul {
+    max-width: 327px;
+    padding-top: 64px;
+    padding-bottom: 64px;
+    gap: 120px;
+    text-align: center;
+  }
 }
 </style>
